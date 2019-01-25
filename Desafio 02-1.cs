fa4900f4e00411d2adefed56d528470c3778b5d3
Atrius4 <47016224+Atrius4@users.noreply.github.com>
2019-01-24 21:50:27 -0500

    Add files via upload

---
+++ Desafio 02-1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_02_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese los datos Y,Z");
+
+            double y = double.Parse(Console.ReadLine());
+            double z = double.Parse(Console.ReadLine());
+
+
+            double t = (Math.Sqrt((y * y) + (z * z)));
+            double aRad = (Math.Asin(y / t));
+            double a = (aRad * 180) / Math.PI;
+            double c = (180 - 90 - a);
+
+            Console.WriteLine("t es igual a:" + t);
+            Console.WriteLine("a es igual a:" + a);
+            Console.WriteLine("c es igual a:" + c);
+        }
+    }
+}
