ef39f4c83129dfd6d65366c0e72d20dcc7784ab5
Andrés Muñoz Ramírez <47016224+Atrius4@users.noreply.github.com>
2019-01-30 07:55:26 -0500

    Add files via upload

---
+++ FórmulaGeneral.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese los coheficientes de las variables(a,b,c)");
+
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+            double dis = (b * b) - (4 * a * c);
+
+            if (dis == 0)
+            {
+                double x = (-b) / (2 * a);
+                Console.WriteLine("La variable x es igual a: " + x);
+            }
+            else if (dis > 0)
+            {
+                double x1 = (-b + Math.Sqrt(dis)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(dis)) / (2 * a);
+                Console.WriteLine("Las posibles soluciones son:" + x1, "ó" + x2);
+            }
+            else if (dis < 0)
+            {
+                Console.WriteLine("No es posible calcular la solución");
+            }
+        }
+    }
+}
---
+++ IMC.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inrese su estatura en metros y su peso en Kilogramos");
+
+            double h = double.Parse(Console.ReadLine());
+            double p = double.Parse(Console.ReadLine());
+            double IMC = p / (h * h);
+
+            if (IMC < 18.5)
+            {
+                Console.WriteLine("Usted Tiene peso inferior al normal");
+            }
+            else if (IMC > 30)
+            {
+                Console.WriteLine("Usted Tiene Obesidad");
+            }
+            else if (IMC > 25)
+            {
+                Console.WriteLine("Usted tiene Sobrepeso");
+            }
+            else
+            {
+                Console.WriteLine("Su peso es adecuado");
+            }
+        }
+    }
+}
