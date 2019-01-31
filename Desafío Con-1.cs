aa3237fa26610054b82fef225714b19586fd2a00
Andrés Muñoz Ramírez <47016224+Atrius4@users.noreply.github.com>
2019-01-31 10:52:06 -0500

    Add files via upload

---
+++ Desafío Con-1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Desafio_Con_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Igrese su salario en COP");
+
+                double salario = double.Parse(Console.ReadLine());
+                double SM = salario/ 828116;
+
+            if (SM < 2)
+            {
+                Console.WriteLine("Su Tarifa es la Tarifa A");
+            }
+            else if (SM < 4)
+            {
+                Console.WriteLine("Su Tarifa es la Tarifa B");
+            }
+            else
+            {
+                Console.WriteLine("Su Tarifa es la Tarifa C");
+            }
+
+
+        }
+    }
+}
---
+++ Desafío Con-2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Desafío_Con_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su salario en COP");
+
+            double sm = double.Parse(Console.ReadLine()) / 828116;
+
+            if (sm < 2)
+            {
+                Console.WriteLine("Según su salario su tarifa es la A: $3200 mensuales");
+            }
+            else if (sm < 5)
+            {
+                Console.WriteLine("Según su salario su tarifa es la B: $12700 mensuales");
+            }
+            else
+            {
+                Console.WriteLine("Según su salario su tarifa es la C: $33500");
+            }
+        }
+    }
+}
