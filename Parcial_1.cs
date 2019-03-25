e417fd5b6f3c2f437258f9243c3ca93320bddb55
Andrés Muñoz Ramírez <47016224+Atrius4@users.noreply.github.com>
2019-03-24 23:27:12 -0500

    Add files via upload

---
+++ Parcial_1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Parcial {
+
+    string[] names;
+    double[] data;
+    public static int codigo = 0; // 0 Primer simulacro. 1 Segundo simulacro
+
+    public Parcial(string[] _names, double[] _data) {
+        data = new double[_data.Length];
+        _data.CopyTo(data, 0);
+        names = new string[_names.Length];
+        _names.CopyTo(names, 0);
+    }
+
+
+    public int PrimerPunto() {
+        string[] nombres = new string[names.Length]; names.CopyTo(nombres, 0);
+        double[] notas = new double[data.Length]; data.CopyTo(notas, 0);
+
+        int salida = 0 ;
+        //--------------------------------------------
+        //- Abajo de esta línea va su código ---------
+        for (int i = 0; i < notas.Length; i++)
+        {
+            if(notas[i] <= 3)
+            {
+                salida += 1;
+            }
+        }
+
+
+        //- Arriba de esta línea va su código --------
+        return salida;
+    }
+
+    public string[] SegundoPunto() {
+        string[] nombres = new string[names.Length]; names.CopyTo(nombres, 0);
+        double[] notas = new double[data.Length]; data.CopyTo(notas, 0);
+
+        string[] salida = new string[5];
+        //--------------------------------------------
+        //- Abajo de esta línea va su código ---------
+
+        for (int i = 0; i < notas.Length; i++)
+        {
+            for (int y = 0; y < notas.Length-1; y++)
+            {
+                if (notas[y] > notas[y + 1])
+                {
+                    double tmp = notas[y + 1];
+                    notas[y + 1] = notas[y];
+                    notas[y] = tmp;
+
+                    string tmp2 = nombres[y + 1];
+                    nombres[y + 1] = nombres[y];
+                    nombres[y] = tmp2;
+                }
+
+            }
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            salida[i] = nombres[i];
+        }
+
+        //- Arriba de esta línea va su código --------
+        return salida;
+    }
+
+    public string[] TercerPunto() {
+        string[] nombres = new string[names.Length]; names.CopyTo(nombres, 0);
+        double[] notas = new double[data.Length]; data.CopyTo(notas, 0);
+        int counter = 0;
+        int guardados = 0;
+
+        //--------------------------------------------
+        //- Abajo de esta línea va su código ---------
+
+        for (int i = 0; i < notas.Length; i++)
+        {
+            if(notas[i] < 3)
+            {
+                counter += 1;
+            }
+        }
+        string[] salida = new string[counter];
+        for (int i = 0; i < notas.Length; i++)
+        {
+            if (notas[i] < 3)
+            {
+                salida[guardados] = nombres[i];
+                guardados++;
+            }
+        }
+        //- Arriba de esta línea va su código --------
+        return salida;
+    }
+
+    public string[] CuartoPunto() {
+        string[] nombres = new string[names.Length]; names.CopyTo(nombres, 0);
+        double[] notas = new double[data.Length]; data.CopyTo(notas, 0);
+        int counter = 0;
+        int guardado = 0;
+        string[] nombresGuardados = new string[nombres.Length];
+        
+        //--------------------------------------------
+        //- Abajo de esta línea va su código ---------
+        for (int i = 0; i < nombres.Length; i++)
+        {
+            string name = nombres[i].ToUpper();
+            for (int y = 0; y < name.Length; y++)
+            {
+                char letra = 'A';
+                if(name[y] == letra)
+                {
+                    counter++;
+                    nombresGuardados[guardado] = name;
+                    guardado++;
+                    break;
+                }
+            }
+        }
+        string[] salida = new string[counter];
+        for (int i = 0; i < salida.Length; i++)
+        {
+            salida[i] = nombresGuardados[i];
+        }
+
+
+
+        //- Arriba de esta línea va su código --------
+        return salida;
+    }
+}
+
+
---
+++ Parcial_2.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Parcial {
+
+    string[] names;
+    double[] data;
+    public static int codigo = 1; // 0 Primer simulacro. 1 Segundo simulacro
+
+    public Parcial(string[] _names, double[] _data) {
+        data = new double[_data.Length];
+        _data.CopyTo(data, 0);
+        names = new string[_names.Length];
+        _names.CopyTo(names, 0);
+    }
+
+
+    public int PrimerPunto() {
+        string[] nombres = new string[names.Length]; names.CopyTo(nombres, 0);
+        double[] notas = new double[data.Length]; data.CopyTo(notas, 0);
+        double sumatoria = 0;
+        int salida = 0 ;
+        double promedio = 0;
+        //--------------------------------------------
+        //- Abajo de esta línea va su código ---------
+        for (int i = 0; i < notas.Length; i++)
+        {
+            sumatoria += notas[i];
+        }
+        promedio = sumatoria / notas.Length;
+        for (int i = 0; i < notas.Length; i++)
+        {
+            if (notas[i] < promedio)
+            {
+                salida++;
+            }
+        }
+
+
+        //- Arriba de esta línea va su código --------
+        return salida;
+    }
+
+    public string[] SegundoPunto() {
+        string[] nombres = new string[names.Length]; names.CopyTo(nombres, 0);
+        double[] notas = new double[data.Length]; data.CopyTo(notas, 0);
+        double sumatoria = 0, promedio =0;
+
+        string[] salida = new string[5];
+        //--------------------------------------------
+        //- Abajo de esta línea va su código ---------
+        for (int i = 0; i < notas.Length; i++)
+        {
+            sumatoria += notas[i];
+        }
+        promedio = sumatoria / notas.Length;
+        for (int i = 0; i < notas.Length; i++)
+        {
+            for (int y = 0; y < notas.Length-1; y++)
+            {
+                if (Math.Abs(notas[y] - promedio) > Math.Abs(notas[y+1] - promedio))
+                {
+                    double tmp = notas[y + 1];
+                    notas[y + 1] = notas[y];
+                    notas[y] = tmp;
+
+                    string tmp2 = nombres[y + 1];
+                    nombres[y + 1] = nombres[y];
+                    nombres[y] = tmp2;
+                }
+            }
+            
+        }
+        for (int i = 0; i < salida.Length; i++)
+        {
+            salida[i] = nombres[i];
+        }
+
+        
+
+        //- Arriba de esta línea va su código --------
+        return salida;
+    }
+
+    public string[] TercerPunto() {
+        string[] nombres = new string[names.Length]; names.CopyTo(nombres, 0);
+        double[] notas = new double[data.Length]; data.CopyTo(notas, 0);
+        int counter = 0;
+        int guardados = 0;
+        double sumatoria = 0, promedio = 0;
+
+
+        //--------------------------------------------
+        //- Abajo de esta línea va su código ---------
+
+        for (int i = 0; i < notas.Length; i++)
+        {
+            sumatoria += notas[i];
+        }
+        promedio = sumatoria / notas.Length;
+        for (int i = 0; i < notas.Length; i++)
+        {
+            if(notas[i] > promedio)
+            {
+                counter += 1;
+            }
+        }
+        string[] salida = new string[counter];
+        for (int i = 0; i < notas.Length; i++)
+        {
+            if (notas[i] > promedio)
+            {
+                salida[guardados] = nombres[i];
+                guardados++;
+            }
+        }
+        //- Arriba de esta línea va su código --------
+        return salida;
+    }
+
+    public string[] CuartoPunto() {
+        string[] nombres = new string[names.Length]; names.CopyTo(nombres, 0);
+        double[] notas = new double[data.Length]; data.CopyTo(notas, 0);
+        string[] nombresGuardados = new string[nombres.Length];
+        int counter = 0, guardados = 0;
+        char[] vocales = { 'A', 'E', 'I', 'O', 'U' };
+        //--------------------------------------------
+        //- Abajo de esta línea va su código ---------
+        for (int i = 0; i < nombres.Length; i++)
+        {
+            string name = nombres[i];
+
+            for (int y = 0; y < vocales.Length; y++)
+            {
+                if(name[0] == vocales[y])
+                {
+                    counter++;
+                    nombresGuardados[guardados] = name;
+                    guardados++;
+                    break;
+                }
+
+            }
+        }
+        string[] salida = new string[counter];
+        for (int i = 0; i < counter; i++)
+        {
+            salida[i] = nombresGuardados[i];
+        }
+
+        
+
+
+        //- Arriba de esta línea va su código --------
+        return salida;
+    }
+}
+
+
